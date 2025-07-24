27e2480d2dafb0c73ab190a5d30230abf826c99b
RituLog <ritu.k.gupta67@gmail.com>
2025-07-24 20:10:51 +0530

    array basic questions

---
+++ src/com/array/InsertElementAtPos.java
@@ -0,0 +1,47 @@
+package com.array;
+
+public class InsertElementAtPos {
+        public static void insertEleSpecificPos(int[] a, int pos, int element) {
+            // Create a new array with one more space
+            int[] newArray = new int[a.length + 1];
+
+            // Copy elements up to the insertion position
+            for (int i = 0; i < pos - 1; i++) {
+                newArray[i] = a[i];
+            }
+
+            // Insert the new element
+            newArray[pos - 1] = element;
+
+            // Copy the remaining elements
+            for (int i = pos; i < newArray.length; i++) {
+                newArray[i] = a[i - 1];
+            }
+
+            // Print the new array
+            for (int i = 0; i < newArray.length; i++) {
+                System.out.println(newArray[i]);
+            }
+        }
+
+        //inser element at specific index position
+        public static void inserElementatIndexSpecific(int[] a, int index_pos, int element){
+            for(int i=a.length-1; i>index_pos; i--){
+                a[i]=a[i-1];
+            }
+            a[index_pos]=element;
+            for(int i=0; i<a.length; i++){
+                System.out.println(a[i] +" ");
+            }
+        }
+        public static void main(String[] args) {
+            int[] a = {10, 20, 30, 40, 50};
+            int pos = 3; // Position to insert
+            int element = 100; // Element to insert
+            insertEleSpecificPos(a, pos, element);
+            System.out.println();
+            inserElementatIndexSpecific(a,pos,element);
+        }
+    }
+
+
---
+++ src/com/array/MergeTwoArray.java
@@ -0,0 +1,36 @@
+package com.array;
+
+public class MergeTwoArray {
+
+    // Brute force method to merge two arrays
+    public static int[] mergeArrays(int[] a, int[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+        int[] merged = new int[aLen + bLen];
+
+        // Copy elements from the first array
+        for (int i = 0; i < aLen; i++) {
+            merged[i] = a[i];
+        }
+
+        // Copy elements from the second array
+        for (int i = 0; i < bLen; i++) {
+            merged[aLen + i] = b[i];
+        }
+
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        int[] b = {7, 8, 9, 10, 11, 12};
+
+        int[] merged = mergeArrays(a, b);
+
+        System.out.print("Merged using brute force: ");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
